413576db3996dcb1d74fd7f9efd30101b781436d
bridgetcarr2 <122331284+bridgetcarr2@users.noreply.github.com>
2023-05-23 00:10:45 -0400

    Add files via upload

---
+++ flowers.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="code.js"></script>
+</head>
+<body>
+    <h1>Welcome to Bridgets Rose Shop!</h1>
+    <p>
+        Shop locally grown flower arrangements! Shop our vast variety of rose bushes. 
+        We are a Family Owned Business!
+    </p>
+    
+</body>
+</html>
